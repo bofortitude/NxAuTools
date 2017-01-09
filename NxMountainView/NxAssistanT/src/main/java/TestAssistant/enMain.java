c7cf384c30d2c58e61a8d34c1d65204ed7450bc3
root <christyler@126.com>
2017-01-08 16:27:09 -0800

    make changes

---
+++ NxMountainView/NxAssistanT/src/main/java/TestAssistant/enMain.java
@@ -0,0 +1,26 @@
+package TestAssistant;
+
+/**
+ * Created by root on 1/3/17.
+ */
+
+import org.apache.logging.log4j.*;
+
+import javax.swing.*;
+import java.io.File;
+import java.io.IOException;
+
+
+public class enMain {
+    private static Logger logger = LogManager.getLogger(LogManager.ROOT_LOGGER_NAME);
+    public static void main(String [] args){
+
+
+
+
+
+
+
+    }
+
+}
